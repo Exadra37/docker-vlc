7d57fadaf549f12020d0c2157553d956849a5a0d
Exadra37 <exadra37@gmail.com>
2018-05-20 16:09:27 +0100

    Mapping vlc config folder from host into the docker container.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- bin/vlc.sh
+++ bin/vlc.sh
@@ -4,12 +4,17 @@
 	# -v ~/.pulse:/home/vlc/.pulse \
 	# -v /dev/shm:/dev/shm \
 
+VLC_HOST_DIR=~/.docker-vlc/.config
+
+[ -d $VLC_HOST_DIR ] ||  mkdir -p $VLC_HOST_DIR
+ 
 sudo docker run \
 	--rm \
 	--detach \
 	--name vlc \
 	-v /etc/localtime:/etc/localtime:ro \
 	-v $PWD:/home/vlc/videos \
+	-v $VLC_HOST_DIR:/home/vlc/.config \
 	--device /dev/snd \
 	--device /dev/dri \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
